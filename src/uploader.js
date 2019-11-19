91a42624a3b4bd82f5c4777326feb0f9bc15699e
Eric Maro <eric.j.maro@gmail.com>
2019-11-19 19:00:48 +0300

    missing package

--- src/uploader.js
+++ src/uploader.js
@@ -23,7 +23,7 @@ export default class Uploader {
   uploadSelectedFile({ onPreview }) {
     ajax.transport({
       url: this.config.endpoint,
-      headers:this.config.headers,
+      headers: this.config.headers,
       accept: this.config.types,
       beforeSend: () => onPreview(),
       fieldName: this.config.field
