d0e065b7d0fd7d7834822510fc97814b7a61ff65
Eric Maro <eric.j.maro@gmail.com>
2019-11-20 08:12:59 +0300

    headers fix

--- src/index.js
+++ src/index.js
@@ -77,6 +77,7 @@ export default class AttachesTool {
 
     this.config = {
       endpoint: config.endpoint || '',
+      headers: config.headers || '',
       field: config.field || 'file',
       types: config.types || '*',
       buttonText: config.buttonText || 'Select file to upload',
